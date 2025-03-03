b2fc0ae3a8dc5ad84822b4db3f59be311df8b387
iMaxim14 <i.maxim.prog@gmail.com>
2025-03-03 15:58:40 +0100

    update

--- src/main/java/homework07/Homework07.java
+++ src/main/java/homework07/Homework07.java
@@ -1,23 +1,45 @@
 package homework07;
 
+import lombok.extern.slf4j.Slf4j;
+
+//@Slf4j
 public class Homework07 {
-    public int add(int a, int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public boolean isEven (int a){
+    public boolean isEven(int a) {
         return a % 2 == 0;
     }
 
-    public int divide (int a, int b){
+    public int divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Divide by zero");
+
+            return -111;
+        }
         return a / b;
     }
 
-    public int getLength (String a){
+    public int getLength(String a) {
+        if (a == null) {
+            System.out.println("String is null");
+            return -1;
+        }
         return a.length();
     }
 
-    public boolean containsWord(String text, String word){
+    public boolean containsWord(String text, String word) {
+        if (text == null || word == null) {
+            System.out.println("Text or word is null");
+            return false;
+        }
+        if (text.isEmpty() || word.isEmpty()) {
+            System.out.println("Text or word is empty");
+            return false;
+        }
         return text.contains(word);
     }
+
 }
